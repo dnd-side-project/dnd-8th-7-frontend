4ccf53e9472f3b16cfd93a06b1fbea126218ad9d
yoonncho <nicealice99@gmail.com>
2023-02-07 00:07:39 +0900

    chore: 폴더 구조 정리(#1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      black: '#333333',
+      red: '#FF7154',
+      blue: '#6EA8FF',
+      purple: '#8B81FF',
+    },
     extend: {},
   },
   plugins: [],
