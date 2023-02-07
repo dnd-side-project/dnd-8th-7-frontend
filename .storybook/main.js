3d6b88ca8448bf912a285b5317b4d090ed02b434
yoonncho <nicealice99@gmail.com>
2023-02-07 20:26:26 +0900

    chore: storybook path alias 설정 추가(#1)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -14,6 +16,13 @@ module.exports = {
     },
   ],
   framework: '@storybook/react',
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+    }
+    return config
+  },
   core: {
     builder: '@storybook/builder-webpack5',
   },
