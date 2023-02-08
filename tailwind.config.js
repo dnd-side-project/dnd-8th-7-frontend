8f3cf372f46f7f199334b6dfd030baa4b944cf7d
yoonncho <nicealice99@gmail.com>
2023-02-08 14:11:42 +0900

    chore: tailwind 컬러 시스템 및 fontSize 정의(#1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,20 @@ module.exports = {
     colors: {
       black: '#333333',
       red: '#FF7154',
-      blue: '#6EA8FF',
-      purple: '#8B81FF',
+      orange: '#FFB673',
+      skyblue: '#7CCAE2',
+      blue: '#5B9DFF',
+      purple: '#7E85FF',
     },
-    extend: {},
+    extend: {
+      fontSize: {
+        sm: ['12px', '130%'],
+        base: ['14px', '130%'],
+        lg: ['16px', '140%'],
+        xl: ['18px', '140%'],
+        '2xl': ['20px', '140%'],
+      },
+    },
   },
   plugins: [],
 }
