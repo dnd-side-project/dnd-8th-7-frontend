5a1e0911efbe317611919e8269c2dc1ce53ea8da
yoonncho <nicealice99@gmail.com>
2023-02-12 01:47:09 +0900

    feat: 공통 Button 컴포넌트 구현(#3)

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -1,11 +1,27 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
-import Button from '@/components/Button'
+import Button, { sizeOptions, colorOptions } from '.'
 
 export default {
   title: 'components/Button',
   components: Button,
+  argTypes: {
+    size: {
+      options: sizeOptions,
+      control: { type: 'radio' },
+    },
+    color: {
+      options: colorOptions,
+      control: { type: 'radio' },
+    },
+  },
 } as ComponentMeta<typeof Button>
 
-const Template: ComponentStory<typeof Button> = () => <Button />
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
 
 export const Basic = Template.bind({})
+Basic.args = {
+  children: '기본 버튼',
+  disabled: false,
+  size: 'sm',
+  color: 'black',
+}
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,54 @@
-const Button = () => {
-  return <button className="bg-red">버튼</button>
+import clsx from 'clsx'
+import { PropsWithChildren } from 'react'
+
+export const colorOptions = ['white', 'gray', 'black'] as const
+export const sizeOptions = ['sm', 'md'] as const
+interface Props extends React.ComponentPropsWithoutRef<'button'> {
+  size?: (typeof sizeOptions)[number]
+  color?: (typeof colorOptions)[number]
+  disabled?: boolean
+}
+
+const COLOR_CONFIG = {
+  white: 'bg-white text-gray-300',
+  gray: 'bg-gray-100 text-gray-300',
+  black: 'bg-black text-white',
+}
+
+const SIZE_CONFIG = {
+  sm: 'px-3 py-1 w-fit rounded-sm',
+  md: 'px-8 py-[13px] w-full rounded-lg',
+}
+
+const Button = ({
+  type = 'button',
+  color = 'black',
+  disabled = false,
+  children,
+  size = 'md',
+  ...rest
+}: PropsWithChildren<Props>) => {
+  const { onClick } = rest
+
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={!!disabled}
+      className={clsx(
+        'flex',
+        'items-center',
+        'justify-center',
+        COLOR_CONFIG[color],
+        SIZE_CONFIG[size],
+        {
+          'disabled:text-gray-200 disabled:bg-gray-100': color === 'black',
+        },
+      )}
+    >
+      {children}
+    </button>
+  )
 }
 
 export default Button
