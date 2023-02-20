0e4dcd393b8f2db92ec3ee59519ec9bc4bce2cdb
eunseonglee <eunseongofficial@gmail.com>
2023-02-21 04:06:57 +0900

    feat: 프로필 생성/조회 페이지 생성(#25)

---
+++ src/containers/Profile/MyProfile/index.tsx
@@ -0,0 +1,3 @@
+export default function MyProfileContainer() {
+  return <div>my profile</div>
+}
---
+++ src/containers/Profile/New/index.tsx
@@ -0,0 +1,3 @@
+export default function NewProfileContainer() {
+  return <div>new profile</div>
+}
