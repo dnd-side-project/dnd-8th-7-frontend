ec7a1d5d789adb6e8345e4824c6d9866524f2ee2
eunseonglee <eunseongofficial@gmail.com>
2023-02-21 00:53:35 +0900

    feat: Input 스토리북 수정(#23)

--- src/components/Input/index.stories.tsx
+++ src/components/Input/index.stories.tsx
@@ -1,32 +1,32 @@
-import { ComponentMeta } from '@storybook/react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 import Input from '@/components/Input'
 import BlockInput from '@/components/Input/BlockInput'
 import { noop } from '@/utils'
 
 export default {
-  title: 'components/BlockInput',
+  title: 'components/Input',
   components: BlockInput,
-} as ComponentMeta<typeof BlockInput>
+} as ComponentMeta<typeof Input>
 
-export const Basic = () => (
-  <Input
-    {...{
-      showLimitCount: true,
-      maxLength: 20,
-      defaultValue: '',
-      placeholder: '기본 Input 입니다',
-    }}
-  />
+const BasicTemplate: ComponentStory<typeof Input> = (args) => (
+  <Input {...args} />
 )
+export const Basic = BasicTemplate.bind({})
+Basic.args = {
+  showLimitCount: true,
+  maxLength: 20,
+  defaultValue: '',
+  placeholder: '기본 Input 입니다',
+}
 
-export const Block = () => (
-  <BlockInput
-    {...{
-      showLimitCount: true,
-      maxLength: 15,
-      defaultValue: '',
-      placeholder: '블록 제목 생성 Input 입니다',
-      onEmojiChange: noop,
-    }}
-  />
+const BlockTemplate: ComponentStory<typeof BlockInput> = (args) => (
+  <BlockInput {...args} />
 )
+export const Block = BlockTemplate.bind({})
+Block.args = {
+  showLimitCount: true,
+  maxLength: 15,
+  defaultValue: '',
+  placeholder: '블록 제목 생성 Input 입니다',
+  onEmojiChange: noop,
+}
