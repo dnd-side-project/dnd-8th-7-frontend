7700907446ac9d2ce91886054c6f39513df9fca3
yoonncho <nicealice99@gmail.com>
2023-02-24 21:13:22 +0900

    feat: DailyBlockPanel에 새 블럭 추가하기 페이지 연결(#22)

--- src/containers/Home/DailyBlockPanel/index.tsx
+++ src/containers/Home/DailyBlockPanel/index.tsx
@@ -1,54 +1,60 @@
+import { useEffect } from 'react'
+import dayjs from 'dayjs'
+import { MOCK_WEEKLY_BLOCKS } from '@/constants/block'
 import Button from '@/components/Button'
-import Date from '@/components/Date'
 import { AddIcon } from '@/components/Icons'
-import Block from '@/components/Block'
-import NoData from '@/components/NoData'
-import { MOCK_BLOCK_LIST, MOCK_WEEKLY_BLOCKS } from '@/constants/block'
+import useSelectedDateState from '@/store/selectedDate'
 import WeeklyBlocks from './WeeklyBlocks'
+import BlockList from '../BlockList'
+
+import rnWebViewBridge from '@/utils/react-native-webview-bridge/new-webview/rnWebViewBridge'
+
+const AddButton = ({
+  text,
+  onClick,
+}: {
+  text: string
+  onClick: () => void
+}) => {
+  return (
+    <Button
+      color="gray"
+      fontWeight="bold"
+      className="py-[11px]"
+      onClick={onClick}
+    >
+      <AddIcon className="!fill-textGray-200" width={18} height={18} />
+      {text}
+    </Button>
+  )
+}
 
 const DailyBlockPanel = () => {
-  const { date, totalBlock, totalTask, blocks } = MOCK_BLOCK_LIST
+  const setSelectedDate = useSelectedDateState((state) => state.setSelectedDate)
+  useEffect(() => {
+    const today = String(dayjs().format('YYYY-MM-DD'))
+    setSelectedDate(today)
+  }, [setSelectedDate])
 
-  const handleBlockCreate = () => {
-    // 블럭 생성 페이지로 이동
+  const handleAddSavedBlock = () => {} // TODO: 페이지 연결
+
+  const handleAddNewBlock = () => {
+    rnWebViewBridge.open({
+      key: 'newBlock',
+      url: 'http://localhost:3000/blocks/new',
+    })
   }
 
   return (
-    <div className="px-5 mt-7">
+    <div className="px-5">
       <WeeklyBlocks weeklyBlocks={MOCK_WEEKLY_BLOCKS} />
-      <Date date={date} totalBlock={totalBlock} totalTask={totalTask} />
-      <div className="mt-[18px] mb-5">
-        {blocks.length === 0 ? (
-          <>
-            <NoData outlined />
-            <Button
-              rounded="lg"
-              className="!py-[15px] mt-5"
-              onClick={handleBlockCreate}
-            >
-              <AddIcon width={18} height={18} className="!fill-white" />
-              <p className="ml-px">새 블럭 만들기</p>
-            </Button>
-          </>
-        ) : (
-          blocks.map(
-            ({ color, icon, title, sumOfTask, sumOfDoneTask, tasks }, idx) => (
-              <div key={idx} className="mb-2">
-                <Block
-                  color={color}
-                  icon={icon}
-                  title={title}
-                  sumOfTask={sumOfTask}
-                  sumOfDoneTask={sumOfDoneTask}
-                  tasks={tasks}
-                />
-              </div>
-            ),
-          )
-        )}
+      <BlockList />
+
+      <div className="flex gap-2">
+        <AddButton text="블럭 불러오기" onClick={handleAddSavedBlock} />
+        <AddButton text="새 블럭 만들기" onClick={handleAddNewBlock} />
       </div>
     </div>
   )
 }
-
 export default DailyBlockPanel
