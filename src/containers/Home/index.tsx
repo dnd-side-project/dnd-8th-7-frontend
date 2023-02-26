6c3724f8533d89770e1f1df5a546d0ce79b8f5d3
yoonncho <nicealice99@gmail.com>
2023-02-26 21:22:00 +0900

    feat: Tab의 Panel 영역 변경 및 DailyBlock 데이터 불러오기(#42)

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,14 +1,73 @@
+import { useEffect, useState } from 'react'
+import dayBlockAPI from '@/api'
+import { GetDailyBlocksOnWeekResponse } from '@/api/types'
 import Tabs from '@/components/Tabs'
+import Button from '@/components/Button'
+import { AddIcon } from '@/components/Icons'
+import { BASE_URL } from '@/constants/urls'
 import ProfileHeader from './ProfileHeader'
 import CalendarPanel from './CalendarPanel'
 import DailyBlockPanel from './DailyBlockPanel'
+import BlockList from './BlockList'
 
-const USER_NICKNAME = '김새롬' // TODO : 서버 데이터 연결
+import rnWebViewBridge from '@/utils/react-native-webview-bridge/new-webview/rnWebViewBridge'
+
+const AddButton = ({
+  text,
+  onClick,
+}: {
+  text: string
+  onClick: () => void
+}) => {
+  return (
+    <Button
+      color="gray"
+      fontWeight="bold"
+      className="py-[11px]"
+      onClick={onClick}
+    >
+      <AddIcon className="!fill-textGray-200" width={18} height={18} />
+      {text}
+    </Button>
+  )
+}
 
 const Home = () => {
+  const [data, setData] = useState<GetDailyBlocksOnWeekResponse | null>(null)
+  const handleBlockCreate = () => {
+    // 블럭 생성 페이지로 이동
+  }
+  const DATE = '2023-02-26'
+
+  useEffect(() => {
+    const getDailyBlocks = async () => {
+      await dayBlockAPI
+        .getDailyBlocksOnWeek({ date: DATE })
+        .then(({ data }) => setData(data))
+    }
+    getDailyBlocks()
+  }, [])
+
+  const handleAddSavedBlock = () => {
+    rnWebViewBridge.open({
+      key: 'savedBlock',
+      url: `${BASE_URL}/blocks/saved`,
+    })
+  }
+
+  const handleAddNewBlock = () => {
+    rnWebViewBridge.open({
+      key: 'newBlock',
+      url: `${BASE_URL}/blocks/new`,
+    })
+  }
+
+  if (!data) return null
+  const { user, dailyBlocks } = data
+
   return (
     <div className="inner">
-      <ProfileHeader user={USER_NICKNAME} />
+      <ProfileHeader user={user} />
 
       <Tabs>
         <Tabs.TabList>
@@ -16,12 +75,19 @@ const Home = () => {
           <Tabs.Tab id={1} text="달력" />
         </Tabs.TabList>
         <Tabs.TabPanel id={0}>
-          <DailyBlockPanel />
+          <DailyBlockPanel dailyBlocks={dailyBlocks} />
         </Tabs.TabPanel>
         <Tabs.TabPanel id={1}>
           <CalendarPanel />
         </Tabs.TabPanel>
       </Tabs>
+
+      <BlockList />
+
+      <div className="flex gap-2">
+        <AddButton text="블럭 불러오기" onClick={handleAddSavedBlock} />
+        <AddButton text="새 블럭 만들기" onClick={handleAddNewBlock} />
+      </div>
     </div>
   )
 }
