3445b385bf05039483f6782d005d8fe0067f6541
eunseonglee <eunseongofficial@gmail.com>
2023-03-01 01:07:43 +0900

    feat: 프로필 생성 기능 구현(#55)

--- src/containers/Profile/New/index.tsx
+++ src/containers/Profile/New/index.tsx
@@ -1,28 +1,78 @@
-import { useRouter } from 'next/router'
+import { ChangeEvent, useState } from 'react'
 import clsx from 'clsx'
 
-import { PATH } from '@/constants/path'
+import useHttpRequest from '@/hooks/useHttpRequest'
 import rnWebViewBridge from '@/utils/react-native-webview-bridge/new-webview/rnWebViewBridge'
+import { dayBlockAPI } from '@/api'
+import { UpdateMyProfileParams } from '@/api/types/base.types'
 
+import ProfileForm from '@/containers/Profile/ProfileForm'
 import Header from '@/components/Header'
 import BottomButtonLayout from '@/components/Layout/BottomButton'
-import ProfileForm from '@/containers/Profile/ProfileForm'
+import { BASE_URL } from '@/constants/urls'
+import { PATH } from '@/constants/path'
 
 export default function NewProfileContainer() {
-  const router = useRouter()
+  const [, updateProfile, isUpdateLoading] = useHttpRequest(
+    (params: UpdateMyProfileParams) =>
+      dayBlockAPI.updateMyProfile(params).then(({ data }) => data),
+  )
 
+  const [profileImage, setProfileImage] = useState('')
+  const [nickname, setNickname] = useState('')
+  const [introduction, setIntroduction] = useState('')
+  const [isSecret, setIsSecret] = useState(false)
+
+  const handleProfileImageChange = (url: string) => {
+    setProfileImage(url)
+  }
+
+  const handleNickNameChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    setNickname(target.value)
+  }
+  const handleDescritionChange = ({
+    target,
+  }: ChangeEvent<HTMLInputElement>) => {
+    setIntroduction(target.value)
+  }
+  const handlePublicNickNameChange = (value: boolean) => {
+    setIsSecret(value)
+  }
   const handleGoBack = () => {
     rnWebViewBridge.close()
   }
 
   const handleSubmit = () => {
-    router.push(PATH.newProfileDone)
+    updateProfile(
+      {
+        imgPath: profileImage,
+        user: nickname,
+        introduction: introduction,
+        lock: isSecret,
+      },
+      {
+        onSuccess: () => {
+          rnWebViewBridge.open({
+            key: 'newProfileDone',
+            url: BASE_URL + PATH.newProfileDone,
+            reset: true,
+          })
+        },
+        onError: () => {
+          // TODO 에러 메세지 전달
+        },
+      },
+    )
   }
 
   return (
     <BottomButtonLayout
       buttonText="완료"
-      buttonProps={{ onClick: handleSubmit }}
+      buttonProps={{
+        onClick: handleSubmit,
+        disabled:
+          isUpdateLoading || !(profileImage && nickname && introduction),
+      }}
     >
       <Header title={''} leftButton={'back'} onLeftButtonClick={handleGoBack} />
       <div className={clsx('pt-[56px]', 'px-[20px]')}>
@@ -39,7 +89,15 @@ export default function NewProfileContainer() {
           <br />
           프로필을 등록해보세요
         </div>
-        <ProfileForm />
+        <ProfileForm
+          nickNameProps={{ onChange: handleNickNameChange }}
+          descriptionPrps={{ onChange: handleDescritionChange }}
+          publicNickNameProps={{
+            onChange: handlePublicNickNameChange,
+            defaultValue: false,
+          }}
+          onProfileImageChange={handleProfileImageChange}
+        />
       </div>
     </BottomButtonLayout>
   )
--- src/containers/Profile/ProfileForm/index.tsx
+++ src/containers/Profile/ProfileForm/index.tsx
@@ -1,29 +1,88 @@
-import { PropsWithoutRef } from 'react'
+import { PropsWithoutRef, useEffect, useState } from 'react'
 import clsx from 'clsx'
+
+import ProfilePlusSvg from 'public/assets/svgs/profile_plus.svg'
 
 import Profile from '@/components/Profile'
 import Switch, { Props as SwitchProps } from '@/components/Switch'
 import Input, { InputProps } from '@/components/Input'
+import useRNImagePicker from '@/utils/react-native-webview-bridge/image-picker/useImagePicker'
+import webBridge from '@/utils/react-native-webview-bridge'
 
 const LABEL = 'text-lg font-bold text-black mt-[40px] mb-[10px]'
 
 interface Props {
-  onProfileImageChnage?: (file: File) => void
+  onProfileImageChange?: (imageUrl: string) => void
   nickNameProps?: PropsWithoutRef<InputProps>
   descriptionPrps?: PropsWithoutRef<InputProps>
   publicNickNameProps?: SwitchProps
 }
 
 export default function ProfileForm({
-  onProfileImageChnage,
+  onProfileImageChange,
   nickNameProps,
   descriptionPrps,
   publicNickNameProps,
 }: Props) {
+  const openImagePicker = useRNImagePicker('profile')
+
+  const [imageUrl, setImageUrl] = useState<string>()
+
+  const handleProfileImageChangeClick = () => {
+    openImagePicker({
+      onImagePick: (data) => {
+        setImageUrl(data)
+        onProfileImageChange?.(data)
+      },
+    })
+  }
+
+  useEffect(() => {
+    webBridge.init()
+    return () => {
+      webBridge.unmount()
+    }
+  }, [])
+
+  // TODO 특수문자 입력 방지
+
   return (
     <>
       <div className={clsx('flex', 'flex-col', 'items-center', 'mt-[20px]')}>
-        <Profile size="md" />
+        <span
+          onClick={handleProfileImageChangeClick}
+          className={clsx('relative', 'w-[100px]', 'h-[100px]')}
+        >
+          {imageUrl ? (
+            <div
+              className={clsx(
+                'rounded-full',
+                'overflow-hidden',
+                'w-[100px]',
+                'h-[100px]',
+              )}
+            >
+              <img
+                alt="프로필 이미지"
+                src={imageUrl}
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              />
+            </div>
+          ) : (
+            <Profile size="md" />
+          )}
+          <div
+            className={clsx(
+              'absolute',
+              'bottom-0',
+              'right-0',
+              'w-[28px]',
+              'h-[28px]',
+            )}
+          >
+            <ProfilePlusSvg />
+          </div>
+        </span>
       </div>
       <div className={LABEL}>닉네임</div>
       <Input
