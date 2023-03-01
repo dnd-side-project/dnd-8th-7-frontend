0c58f6b1c3bbe57a63d2b2865ea06363304944f5
eunseonglee <eunseongofficial@gmail.com>
2023-03-01 21:14:35 +0900

    fix: webview event 구독하지 않아서 발생하는 버그 수정(#57)

--- src/pages/profiles/new/index.tsx
+++ src/pages/profiles/new/index.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react'
+import webBridge from '@/utils/react-native-webview-bridge'
+
 import NewProfileContainer from '@/containers/Profile/New'
 import { withAuth, withAuthGetServerSideProps } from '@/hoc/withAuth'
 
 const NewProfilePage = () => {
+  useEffect(() => {
+    webBridge.init()
+    return () => {
+      webBridge.unmount()
+    }
+  }, [])
   return <NewProfileContainer />
 }
 export default withAuth(NewProfilePage)
