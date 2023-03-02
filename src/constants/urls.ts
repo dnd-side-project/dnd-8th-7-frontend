1946bd86cffbb3824a39efdcdb765a2d35699cda
eunseonglee <eunseongofficial@gmail.com>
2023-03-02 23:44:54 +0900

    chore: 약관 path 변경

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -2,7 +2,6 @@ export const BASE_URL = process.env.NEXT_PUBLIC_APP_URL
 export const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
 export const DOCS_URL = {
-  termsOfUse: 'https://eunseong.notion.site/4a83eca0a0944f38858bcae1e0552913',
-  privacyPolicy:
-    'https://eunseong.notion.site/1857f35bd1854e69b5da39d7f4f6572c',
+  termsOfUse: BASE_URL + '/doc/policy_of_use.html',
+  privacyPolicy: BASE_URL + '/doc/privacy_policy.html',
 }
