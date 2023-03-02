fac9c0447f88c6b7397ae18773b6d7a398a3c7c1
eunseonglee <eunseongofficial@gmail.com>
2023-03-03 02:19:07 +0900

    feat: api 인터페이스에 없는 타입 추가(#62)

--- src/api/services/DayBlock.interface.ts
+++ src/api/services/DayBlock.interface.ts
@@ -2,21 +2,33 @@ import * as Type from '@/api/types/base.types'
 import { ServiceFunc } from './types'
 
 export default interface DayBlockService {
+  /** 메인 */
   getDailyBlocksOnWeek: ServiceFunc<
     Type.GetDailyBlocksOnWeekParams,
     Type.GetDailyBlocksOnWeekResponse
   >
-
   getDayBlocks: ServiceFunc<Type.GetDayBlocksParams, Type.GetDayBlocksResponse>
 
+  /** 블럭 */
   createBlock: ServiceFunc<Type.CreateBlockParams, Type.CreateBlockResponse>
   deleteBlock: ServiceFunc<Type.DeleteBlockParams, Type.DeleteBlockResponse>
+  updateBlock: ServiceFunc<Type.UpdateBlockParams, Type.UpdateBlockResponse>
 
+  /** 저장 블럭 */
+  getSavedBlocks: ServiceFunc<never, Type.GetSavedBlocksResponse>
+  getSingleBlock: ServiceFunc<
+    Type.GetSingleBlockParams,
+    Type.GetSingleBlockResponse
+  >
+  saveBlock: ServiceFunc<Type.SaveBlockParams, Type.CreateBlockResponse>
+
+  /** 하루 일기 */
   createDailyReview: ServiceFunc<
     Type.CreateDailyReviewParams,
     Type.CreateDailyReviewResponse
   >
 
+  /** 태스크 */
   createTaskInBlock: ServiceFunc<
     Type.CreateTaskInBlockParams,
     Type.CreateTaskInBlockResponse
@@ -30,6 +42,7 @@ export default interface DayBlockService {
     Type.DeleteTaskInBlockResponse
   >
 
+  /** 프로필 */
   updateMyProfile: ServiceFunc<
     Type.UpdateMyProfileParams,
     Type.UpdateMyProfileResponse
