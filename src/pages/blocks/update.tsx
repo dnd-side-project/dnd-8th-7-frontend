8d18f487213dbe5ed19708c436be12b2d29bb109
eunseonglee <eunseongofficial@gmail.com>
2023-03-04 07:08:20 +0900

    fix: webViewBridge init 위치 변경

--- src/pages/blocks/update.tsx
+++ src/pages/blocks/update.tsx
@@ -1,7 +1,15 @@
 import UpdateBlockContainer from '@/containers/UpdateBlock'
 import { withAuth, withAuthGetServerSideProps } from '@/hoc/withAuth'
+import webBridge from '@/utils/react-native-webview-bridge'
+import { useEffect } from 'react'
 
 const UpdateBlockPage = () => {
+  useEffect(() => {
+    webBridge.init()
+    return () => {
+      webBridge.unmount()
+    }
+  }, [])
   return <UpdateBlockContainer />
 }
 export default withAuth(UpdateBlockPage)
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,16 @@
 import HomeContainer from '@/containers/Home'
 import { withAuth, withAuthGetServerSideProps } from '@/hoc/withAuth'
+import webBridge from '@/utils/react-native-webview-bridge'
+import { useEffect } from 'react'
 
 const Home = () => {
+  useEffect(() => {
+    webBridge.init()
+    return () => {
+      webBridge.unmount()
+    }
+  }, [])
+
   return <HomeContainer />
 }
 export default withAuth(Home)
